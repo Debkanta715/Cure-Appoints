557ffc2ad6c8e84039e39dc99cb7657bbc15b1ae
Debkanta <debkantadey29@gmail.com>
2026-05-16 20:34:05 +0530

    ..

--- Frontend/src/components/Topdoctors.jsx
+++ Frontend/src/components/Topdoctors.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {doctors} from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 const Topdoctors = () => {
+  const navigate = useNavigate()  // by this we change the path
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
 
       <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-        {doctors.slice(0,10).map((item , idx)=>(
+        {doctors.slice(0,10).map((item , idx)=>( // by this function we insert the value which is present in the assets
           <div className='bg-white border border-blue-100 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transform transition-transform duration-300 shadow-sm' key={idx}>
             <img className='w-full h-44 object-cover bg-blue-50' src={item.image} alt="" />
             <div className='p-4'>
@@ -24,7 +26,7 @@ const Topdoctors = () => {
       </div>
 
       <div className='w-full flex justify-center'>
-        <button className='mt-6 px-6 py-2 bg-primary text-white rounded-full hover:opacity-90'>more</button>
+        <button onClick={()=>{navigate('/doctors'); window.scrollTo(0,0)}} className='mt-6 px-6 py-2 bg-primary text-white rounded-full hover:opacity-90'>more</button>
       </div>
     </div>
   )
